d27a8420ad325eacded9e788ab9a1fbf53be88ee
Nuria Liaño <lianonuria@gmail.com>
2024-05-30 19:43:10 +0200

    change doc

--- testDocu/docusaurus.config.js
+++ testDocu/docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Skilly docs',
+  tagline: 'Learn is so cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -20,8 +20,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'skilly', // Usually your GitHub org/user name.
+  projectName: 'skilly docu', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Skilly docu',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
