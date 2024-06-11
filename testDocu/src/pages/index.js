da1b7499d3a570c47c6e2a84ed3550ce6381e92d
Nuria Liaño <lianonuria@gmail.com>
2024-06-11 18:42:00 +0200

    modify index

--- testDocu/src/pages/index.js
+++ testDocu/src/pages/index.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import styles from './index.module.css';
-import htmlContent from './custom-home'; // Importamos el contenido del HTML desde custom-home.js
+import CustomHome from './custom-home'; // Importamos el componente CustomHome
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
@@ -11,7 +12,7 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <div className={clsx('container', styles.customHtml)}>
-        <div dangerouslySetInnerHTML={{ __html: htmlContent }} /> {/* Renderizamos el HTML personalizado */}
+        <CustomHome /> {/* Renderizamos el componente CustomHome */}
       </div>
     </Layout>
   );
