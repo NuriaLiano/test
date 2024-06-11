8f3bff4c61d8f1de38e8d5450289dfdfd2c11de1
Nuria Liaño <lianonuria@gmail.com>
2024-06-11 18:20:17 +0200

    fix index

--- testDocu/src/pages/index.js
+++ testDocu/src/pages/index.js
@@ -8,27 +8,7 @@ import Heading from '@theme/Heading';
 import styles from './index.module.css';
 import raw from 'raw.macro'; // Importamos raw.macro para importar el contenido del HTML como texto.
 
-const htmlContent = raw('../../../web/index.html'); // Importamos el contenido del HTML.
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+const htmlContent = raw('../web/index.html'); // Importamos el contenido del HTML.
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
@@ -36,11 +16,9 @@ export default function Home() {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
       <div dangerouslySetInnerHTML={{ __html: htmlContent }} /> {/* Renderizamos el HTML personalizado */}
-        <HomepageFeatures />
-      </main>
+      
+      
     </Layout>
   );
 }
